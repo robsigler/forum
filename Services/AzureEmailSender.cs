e2ed5bac6baaf6a4dc07b507f7987b2a4f3dc354
robrtsql <robrtsql@gmail.com>
2025-05-17 09:45:45 -0500

    Fix formatting

--- Services/AzureEmailSender.cs
+++ Services/AzureEmailSender.cs
@@ -11,16 +11,16 @@ namespace Forum.Services
     public class AzureEmailSender : IEmailSender
     {
         private readonly string _connectionString;
-        
+
         public AzureEmailSender(string connectionString)
         {
             this._connectionString = connectionString;
         }
-        
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             var client = new EmailClient(_connectionString);
-            
+
             var sender = "DoNotReply@robsigler.com";
 
             var emailContent = new EmailContent(subject)
@@ -32,7 +32,7 @@ namespace Forum.Services
             {
                 new EmailAddress(email)
             };
-            
+
             var emailRecipients = new EmailRecipients(toRecipients);
 
             var emailMessage = new EmailMessage(sender, emailRecipients, emailContent);
